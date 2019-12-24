d49567b503a84948cd9159a71ed2abe2d539f8a5
Chris Hundhausen <chundhau@gmail.com>
2019-12-24 13:40:02 -0800

    Code added to clear out My Rounds table

--- scripts/eventHandlers.js
+++ scripts/eventHandlers.js
@@ -7,6 +7,7 @@
   //startUp -- This function sets up the initial state of the app: Login page is
   //visible, bottom bar is invisible, all menu items invisible except feed items,
   //menu bottom disabled, UI mode = login
+  //TO DO: Add code to clear the MyRounds table
   function startUp() {
     //Hide all pages except for Login Page, which is the start page.
     for (var i = 0; i < modes.length; ++i) {
@@ -54,7 +55,22 @@
     //Set default date in log round input form to today:
     document.getElementById("roundDate").valueAsNumber = 
       Date.now()-(new Date()).getTimezoneOffset()*60000;
+
+    //Potentially clear out the "My Rounds" table
+    //Note that we aren't deleting from localStorage since we're prepping for
+    //new login.
+    let roundsTable = document.getElementById("myRoundsTable");
+    if (!roundsTable.rows[1].innerHTML.includes ("colspan")) {
+      //We have a non-empty table and must clear out rows
+      while (roundsTable.rows.length > 1) {
+        roundsTable.deleteRow(1);
+      }
+      //Add new row to indicate empty table.
+      let newRow = roundsTable.insertRow();
+      newRow.innerHTML = "<td colspan='5' style='font-style: italic'>No rounds logged</td>"; 
+    }
   }
+
 
   //document click: When the user clicks anywhere in the doc and the menu is open
   //we need to close it and toggle menu state variable.
@@ -363,6 +379,52 @@ function addToOrUpdateRoundTable(add, roundIndex) {
    "<span class='fas fa-trash'></span></button></td>";
 }
 
+//confirmDelete: Event handler called when "Delete" button clicked in "My Rounds"
+//table. roundIndex indicates the index of the round tht was clicked. We presenta
+//modal dialog box asking user to confirm the deletion. The confirm button event
+//handler calls on the function deleteRound, which does the deletion.
+function confirmDelete(roundIndex) {
+  //Preserve index of round to delete for deleteRound function
+  localStorage.setItem("pendingDelete",roundIndex); 
+  //Show the modal dialog box
+  document.getElementById("deleteRoundModal").style.display = "block";
+}
+
+//cancelDelete: Event handler called when "No, do not delete" button clicked in
+//the confirm delete modal dialog. In this case, we simply hide the dialog box
+//aclear out the "pendingDelete" local storage item
+function cancelDelete() {
+  localStorage.setItem("pendingDelete","");
+  document.getElementById("deleteRoundModal").style.display = "none";
+}
+
+//deleteRound: Event handler called when "Yes, delete round" button clicked in
+//confirm delete dialog box. We fetch the index of round to delete from local
+//storage and delete the corresponding row from the table and record from the
+//rounds array. We also hide the dialog box.
+function deleteRound() {
+  //Hide modal dialog box
+  document.getElementById("deleteRoundModal").style.display = "none";
+  //Grab user data from localStorage
+  let data = JSON.parse(localStorage.getItem("speedgolfUserData"));
+  let user = localStorage.getItem("userName");
+  let roundIndex = Number(localStorage.getItem("pendingDelete"));
+  let row, roundsTable, newRow;
+  //delete round from rounds associative array and save back to localStorage
+  delete data[user].rounds[roundIndex];
+  localStorage.setItem("speedgolfUserData",JSON.stringify(data));
+  //delete the row from the table
+  row = document.getElementById("r-" + roundIndex);
+  row.parentNode.removeChild(row);
+  //If we're now down to just header row, we need to add a row saying that no
+  //rounds have been added yet
+  roundsTable = document.getElementById("myRoundsTable");
+  if (roundsTable.rows.length == 1) {
+    //Add new row
+    newRow = roundsTable.insertRow();
+    newRow.innerHTML = "<td colspan='5' style='font-style: italic'>No rounds logged</td>"; 
+  }
+}
 
 //MENU BUTTON HANDLERS GO HERE
 
@@ -393,6 +455,8 @@ document.getElementById("logRoundItem").onclick = function(e) {
   document.getElementById("logRoundDiv").style.display = "block";
   //Change page title:
   document.getElementById("topBarTitle").textContent = "Log New Round";
+  //Set label of form button appropriately
+  document.getElementById("submitBtnLabel").textContent = "Save Round Data";
   //Set pageLocked to true, thus indicating that we're on a page that may only
   //be exited by clicking on the left arrow at top left
   pageLocked = true;
